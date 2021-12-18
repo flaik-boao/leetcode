1a29a84cfb761e6f6cbe26d20560ee191b0add08
Boao Li <liboao1122@hotmail.com>
2021-12-19 00:01:19 +1100

    Update 141.LinkedListCycle.js

--- src/problems/easy/141.LinkedListCycle.js
+++ src/problems/easy/141.LinkedListCycle.js
@@ -11,14 +11,14 @@
  * @return {boolean}
  */
 var hasCycle = function (head) {
-  const nodes = []
+  const nodes = new Set()
   let currentNode = head
 
   if (!currentNode) return false
 
-  while (currentNode.next) {
-    if (nodes.find((_) => _ === currentNode.next)) return true
-    nodes.push(currentNode)
+  while (currentNode) {
+    if (nodes.has(currentNode.next)) return true
+    nodes.add(currentNode.next)
     currentNode = currentNode.next
   }
   return false
